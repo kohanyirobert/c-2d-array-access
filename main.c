4b8065060077676fb4f6084c6fd776e4e423971c
Kohányi Róbert <kohanyi.robert@gmail.com>
2018-09-09 07:17:14 +0200

    Add decayed 2-dimensional array declaration, update printing

--- main.c
+++ main.c
@@ -4,38 +4,55 @@
 #define ROWS 2
 #define COLS 10
 
+int p[ROWS][COLS];
+int (*q)[COLS] = p;
+int *r = *p;
+
 int main(void)
 {
-	int p[ROWS][COLS];
-	int *q = *p;
-
-	printf("rows:         %u\n", ROWS);
-	printf("cols:         %u\n", COLS);
-	printf("rows * cols:  %u\n", ROWS * COLS);
-	printf("sizeof (int): %u\n", sizeof (int));
-	printf("sizeof p:     %u (ROWS * COLS * sizeof (int))\n", sizeof p);
-	printf("sizeof q:     %u\n", sizeof q);
-	printf("sizeof *p:    %u (COLS * sizeof (int))\n", sizeof *p);
-	printf("sizeof *q:    %u (sizeof (int))\n", sizeof *q);
-	printf("p:            %u\n", p);
-	printf("p + 1;        %u\n", p + 1);
-	printf("q:            %u\n", q);
-	printf("q + 1:        %u\n", q + 1);
+	printf("	       ROWS = %u\n", ROWS);
+	printf("	       COLS = %u\n", COLS);
+	printf("        ROWS * COLS = %u\n", ROWS * COLS);
+	putchar('\n');
+	printf("  int p[ROWS][COLS] = ...\n");
+	printf("     int (*q)[COLS] = p\n");
+	printf("	     int *r = *p\n");
+	putchar('\n');
+	printf("       sizeof (int) = %u\n", sizeof (int));
+	putchar('\n');
+	printf("	   sizeof p = %u (ROWS * COLS * sizeof (int))\n", sizeof p);
+	printf("	   sizeof q = %u\n", sizeof q);
+	printf("	   sizeof r = %u\n", sizeof r);
+	putchar('\n');
+	printf("	  sizeof *p = %u (COLS * sizeof (int))\n", sizeof *p);
+	printf("	  sizeof *q = %u\n", sizeof *q);
+	printf("	  sizeof *r = %u (sizeof (int))\n", sizeof *r);
+	putchar('\n');
+	printf("		  p = %u\n", p);
+	printf("	      p + 1 = %u\n", p + 1);
+	putchar('\n');
+	printf("		  q = %u\n", q);
+	printf("	      q + 1 = %u\n", q + 1);
+	putchar('\n');
+	printf("		  r = %u\n", r);
+	printf("	      r + 1 = %u\n", r + 1);
 
 	for (size_t i = 0; i < ROWS; ++i) {
 		for (size_t j = 0; j < COLS; ++j) {
 			/*
-			printf("%u\n", p[i][j]);
-			printf("%u\n", *(*(p + i) + j));
-			printf("%u\n", *(q + i * COLS + j));
+			printf("	    p[%u][%u] = %d\n", i, j, p[i][j]);
+			printf("   *(*(p + %u) + %u)) = %d\n", i, j, *(*(p + i) + j));
+			printf("	    q[%u][%u] = %d\n", i, j, q[i][j]);
+			printf("   *(*(q + %u) + %u)) = %d\n", i, j, *(*(q + i) + j));
+			printf("*(r + %u + COLS + %u) = %d\n", i, j, *(r + i * COLS + j));
 			*/
 		}
 	}
 
 	for (size_t i = 0; i < ROWS * COLS; ++i) {
 		/*
-		printf("%u\n", q[i]);
-		printf("%u\n", *(q + i));
+		printf("	       r[%u] = %d\n", i, r[i]);
+		printf("	   *(r + %u) = %d\n", i, *(r + i));
 		*/
 	}
 
